9fd19ba75c594f9661b5704805175dfed19a638a
김민준 <mjkim-law@naver.com>
2023-05-26 14:44:33 +0900

    :smirk:로그인, 회원가입 및 기타

---
+++ app/src/main/java/com/example/workspace/rogin/Member_joinActivity.kt
@@ -0,0 +1,12 @@
+package com.example.workspace.rogin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.workspace.R
+
+class Member_joinActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_member_join)
+    }
+}
